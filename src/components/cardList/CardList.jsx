fd56ea1ce6df6e2e1eb3ffc5805179d92f69b9dd
mohamedkharma <mohamedkharma@hotmail.com>
2024-02-17 13:13:18 -0500

    trying to fetch

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -1,39 +1,73 @@
-import React from "react";
+"use client"
+import React, { useEffect, useState } from "react";
 import styles from "./cardList.module.css";
 import Pagination from "../pagination/Pagination";
 import Card from "../card/Card";
 
-const getData = async (page, cat) => {
-  const res = await fetch(
-    `http://localhost:3000/api/posts?page=${page}&cat=${cat || ""}`,
-    {
-      cache: "no-store",
+const fetchData = async (page, cat) => {
+  try {
+    const res = await fetch(
+      `/api/posts?page=${page}&cat=${cat || ""}`,
+      {
+        cache: "no-store",
+      }
+    );
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
     }
-  );
 
-  if (!res.ok) {
-    throw new Erro("Failed");
+    return res.json();
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error; // Re-throw the error to be caught by the caller
   }
-
-  return res.json();
 };
 
-const CardList = async ({ page, cat }) => {
-  const { posts, count } = await getData(page, cat);
+const CardList = ({ page, cat }) => {
+  const [posts, setPosts] = useState([]);
+  const [count, setCount] = useState(0);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchDataAsync = async () => {
+      try {
+        setLoading(true);
+        const { posts, count } = await fetchData(page, cat);
+        setPosts(posts);
+        setCount(count);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+
+    fetchDataAsync();
+  }, [page, cat]);
 
   const POST_PER_PAGE = 2;
   const hasPrev = POST_PER_PAGE * (page - 1) > 0;
   const hasNext = POST_PER_PAGE * (page - 1) + POST_PER_PAGE < count;
 
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Recent Posts</h1>
       <div className={styles.posts}>
-        {posts?.map((item) => (
+        {posts.map((item) => (
           <Card item={item} key={item._id} />
         ))}
       </div>
-      <Pagination page={page} hasPrev={hasPrev} hasNext={hasNext} cat={cat}/>
+      <Pagination page={page} hasPrev={hasPrev} hasNext={hasNext} cat={cat} />
     </div>
   );
 };
